d201be15b5a3f73adae8c1e9c19317ead38052cc
Gabriel Jourdenais Hamel <gabrieljourdenaishamel@hotmail.fr>
2016-02-29 15:03:54 -0500

    First

---
+++ classMeet/app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name classMeetApp
+ * @description
+ * # classMeetApp
+ *
+ * Main module of the application.
+ */
+var app=angular
+  .module('classMeetApp', [
+    'ngRoute'
+  ])
+  .config(["$routeProvider",function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ classMeet/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name classMeetApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the classMeetApp
+ */
+angular.module('classMeetApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
